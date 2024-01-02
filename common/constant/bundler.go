f767a98699fc68bb6b647afc0bc933537116e869
jiang <fenghaojiang97@gmail.com>
2024-01-02 11:34:56 +0800

    chore: add builders

--- common/constant/bundler.go
+++ common/constant/bundler.go
@@ -12,14 +12,28 @@ const (
 	GambitLabs       BundlerEndpoint = "https://builder.gmbit.co/rpc/"
 	EthBuilder       BundlerEndpoint = "https://eth-builder.com/"
 	TitanEndpoint    BundlerEndpoint = "https://rpc.titanbuilder.xyz/"
-	BuildAI          BundlerEndpoint = "https://buildai.net"
+	BuildAI          BundlerEndpoint = "https://buildai.net/"
 	Payload          BundlerEndpoint = "https://rpc.payload.de/"
 	Bloxroute        BundlerEndpoint = "https://mev.api.blxrbdn.com/"
 	Lightspeed       BundlerEndpoint = "https://rpc.lightspeedbuilder.info/"
-	NFactorial       BundlerEndpoint = "https://rpc.nfactorial.xyz"
-	BobaBuilder      BundlerEndpoint = "https://boba-builder.com/searcher"
-	F1b              BundlerEndpoint = "https://rpc.f1b.io"
+	NFactorial       BundlerEndpoint = "https://rpc.nfactorial.xyz/"
+	BobaBuilder      BundlerEndpoint = "https://boba-builder.com/searcher/bundle/"
+	F1b              BundlerEndpoint = "https://rpc.f1b.io/"
+	JetBldr          BundlerEndpoint = "https://rpc.jetbldr.xyz/"
+	PenguinBuild     BundlerEndpoint = "https://rpc.penguinbuild.org/"
+	LokiBuilder      BundlerEndpoint = "https://rpc.lokibuilder.xyz/"
+	EdenNetwork      BundlerEndpoint = "https://api.edennetwork.io/v1/bundle/"
+	TBuilder         BundlerEndpoint = "https://rpc.tbuilder.xyz/"
+	Eigenphi         BundlerEndpoint = "https://builder.eigenphi.io/"
+	BlockBeelder     BundlerEndpoint = "https://blockbeelder.com/rpc/"
+	ManifoldFinance  BundlerEndpoint = "https://api.securerpc.com/v1/"
+	PandaBuilder     BundlerEndpoint = "https://rpc.pandabuilder.io/"
+	SmithBot         BundlerEndpoint = "https://smithbot.xyz/"
 )
+
+func (b BundlerEndpoint) String() string {
+	return string(b)
+}
 
 func AllBundlerEndpoints() []BundlerEndpoint {
 	return []BundlerEndpoint{
@@ -38,13 +52,25 @@ func AllBundlerEndpoints() []BundlerEndpoint {
 		NFactorial,
 		BobaBuilder,
 		F1b,
+		JetBldr,
+		PenguinBuild,
+		LokiBuilder,
+		EdenNetwork,
+		TBuilder,
+		Eigenphi,
+		BlockBeelder,
+		ManifoldFinance,
+		PandaBuilder,
+		SmithBot,
 	}
 }
 
 type TestnetBundlerEndpoint string
 
 const (
-	FlashbotsGoerliBundler TestnetBundlerEndpoint = "https://relay-goerli.flashbots.net/"
+	FlashbotsGoerliBundler   TestnetBundlerEndpoint = "https://relay-goerli.flashbots.net/"
+	BuildAIGoerliBundler     TestnetBundlerEndpoint = "https://buildai.net/goerli/"
+	EdenNetworkGoerliBundler TestnetBundlerEndpoint = "https://goerli.edennetwork.io/v1/bundle/"
 )
 
 func (t TestnetBundlerEndpoint) String() string {
